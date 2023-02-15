22171a18355ae0e46a5a8df4b626d3108f891f53
wq-max <53320735+wq-max@users.noreply.github.com>
2023-02-15 21:30:50 +0800

    chapter 7 Builder模式完结

---
+++ chapter7/src/test/HTMLBuilder.java
@@ -0,0 +1,45 @@
+package test;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HTMLBuilder implements Builder {
+    private String filename;
+    private PrintWriter writer;
+    @Override
+    public void makeTitle(String title) {
+        filename = title + ".html";
+        try {
+            writer = new PrintWriter(new FileWriter(filename));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        writer.println("<html><head><meta charset=\"UTF-8\"><title>" + title + "</title></head><body>");
+        writer.println("<h1>" + title + "</h1>");
+    }
+
+    @Override
+    public void makeString(String str) {
+        writer.println("<p>" + str + "</p>");
+    }
+
+    @Override
+    public void makeItems(String[] items) {
+        writer.println("<ul>");
+        for (String item : items) {
+            writer.println("<li>" + item + "</li>");
+        }
+        writer.println("</ul>");
+    }
+
+    @Override
+    public void close() {
+        writer.println("</body></html>");
+        writer.close();
+    }
+
+    public String getResult() {
+        return filename;
+    }
+}
---
+++ chapter7/src/test/TextBuilder.java
@@ -0,0 +1,34 @@
+package test;
+
+public class TextBuilder implements Builder {
+    private StringBuffer buffer = new StringBuffer();
+    @Override
+    public void makeTitle(String title) {
+        buffer.append("============================\n");
+        buffer.append("《").append(title).append("》\n");
+        buffer.append("\n");
+    }
+
+    @Override
+    public void makeString(String str) {
+        buffer.append('#').append(str).append("\n");
+        buffer.append("\n");
+    }
+
+    @Override
+    public void makeItems(String[] items) {
+        for (int i = 0; i < items.length; i++) {
+            buffer.append(" `").append(items[i]).append("\n");
+        }
+        buffer.append("\n");
+    }
+
+    @Override
+    public void close() {
+        buffer.append("============================\n");
+    }
+
+    public String getResult() {
+        return buffer.toString();
+    }
+}
